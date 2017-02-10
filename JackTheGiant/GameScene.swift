5f8cbabde4c1b3e2892f775d17f9bb2cde7d04e3
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-02-09 21:05:08 -0800

    Added a node using Scene Editor and also programatically

--- JackTheGiant/GameScene.swift
+++ JackTheGiant/GameScene.swift
@@ -10,5 +10,10 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    override func didMove(to view: SKView) {
+        let myNode = SKSpriteNode(color: .red, size: CGSize(width: 100, height: 100))
+        myNode.position = CGPoint(x: 0, y: 0)
+        
+//        self.addChild(myNode)
+        }
    }
