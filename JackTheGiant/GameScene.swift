421653d384491424d5ede9af9a6eab6d8cfc0861
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-02-10 17:06:45 -0800

    Add AppIcons

--- JackTheGiant/GameScene.swift
+++ JackTheGiant/GameScene.swift
@@ -21,28 +21,18 @@ class GameScene: SKScene {
         myNode.physicsBody = SKPhysicsBody(circleOfRadius: myNode.size.height/2)
         myNode.physicsBody?.affectedByGravity = false
         
-        
-//        myNode.alpha = 0
-//        myNode.anchorPoint = CGPoint(x: 1, y: 1)   //Anchor point for rotation (1,1) top right corner (0,0) is bottom left
-  
-//        let action = SKAction.fadeIn(withDuration: TimeInterval(5))
-//        let action = SKAction.moveTo(x: 100, duration: TimeInterval(2))
-//        let action = SKAction.move(to: CGPoint(x: 100, y: 200), duration: TimeInterval(2))
-//        let action = SKAction.rotate(byAngle: 2, duration: TimeInterval(2))
-//        myNode.run(action)
-    
-        
-        myNode.position = CGPoint(x: 0, y: 0)
+        myNode.position = CGPoint(x: 0, y: 350)
         myNode.setScale(0.8)
         
         myNode.physicsBody = SKPhysicsBody(circleOfRadius: myNode.size.height/2)
-        myNode.physicsBody?.affectedByGravity = false
+        myNode.physicsBody?.affectedByGravity = true
         
-        node2.position = CGPoint(x: 1, y: 1)
+        node2.position = CGPoint(x: 1, y: -150)
         node2.setScale(0.8)
         
         node2.physicsBody = SKPhysicsBody(circleOfRadius: myNode.size.height/2)
         node2.physicsBody?.affectedByGravity = false
+        node2.physicsBody?.isDynamic = false
         
         self.addChild(myNode)
         self.addChild(node2)
@@ -76,3 +66,26 @@ class GameScene: SKScene {
 //        myNode.position.y += 1     // Moving node up 1 everytime it is called
     }
 }
+
+
+
+
+//        myNode.alpha = 0
+//        myNode.anchorPoint = CGPoint(x: 1, y: 1)   //Anchor point for rotation (1,1) top right corner (0,0) is bottom left
+
+//        let action = SKAction.fadeIn(withDuration: TimeInterval(5))
+//        let action = SKAction.moveTo(x: 100, duration: TimeInterval(2))
+//        let action = SKAction.move(to: CGPoint(x: 100, y: 200), duration: TimeInterval(2))
+//        let action = SKAction.rotate(byAngle: 2, duration: TimeInterval(2))
+//        myNode.run(action)
+
+
+/*  let label = SKLabelNode(fontNamed: "Helvetica")
+    label.text "Player's Score"
+    label.fontSize = 60
+    label.fontColor = SKColor.redColor()
+    label.position = (CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+    self.addChiled(label)
+ 
+    */
+
