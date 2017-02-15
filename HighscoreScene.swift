d2cf187e4e214ccdd543d574e6f9e41e9a4b38e3
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-02-15 12:57:44 -0800

    Add Highscore Scene and Options Scene.  Setup buttons.  Added Highscore Controller and Options Controller

---
+++ HighscoreScene.swift
@@ -0,0 +1,15 @@
+//
+//  HighscoreScene.swift
+//  JackTheGiant
+//
+//  Created by Cynthia Whitlatch on 2/15/17.
+//  Copyright © 2017 Fenix Design. All rights reserved.
+//
+
+import SpriteKit
+
+class Highscore: SKScene {
+    
+    
+    
+}
---
+++ OptionsScene.swift
@@ -0,0 +1,13 @@
+//
+//  OptionsScene.swift
+//  JackTheGiant
+//
+//  Created by Cynthia Whitlatch on 2/15/17.
+//  Copyright © 2017 Fenix Design. All rights reserved.
+//
+
+import SpriteKit
+
+class Options: SKScene {
+    
+}
