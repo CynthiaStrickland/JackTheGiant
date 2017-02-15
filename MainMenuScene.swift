bc5f4add7cce4398d3a15773cdd675d6e6ba8d3b
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-02-15 12:43:28 -0800

    Added Buttons to Main Menu Controller, added animation.

---
+++ MainMenuScene.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenuScene.swift
+//  JackTheGiant
+//
+//  Created by Cynthia Whitlatch on 2/15/17.
+//  Copyright © 2017 Fenix Design. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenu: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+}
