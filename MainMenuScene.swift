d9b6b3b558ea7601907a37cf1ef41db0052bd68b
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-02-16 17:09:18 -0800

    HighscoreButton Pressed printing

--- MainMenuScene.swift
+++ MainMenuScene.swift
@@ -22,32 +22,24 @@ class MainMenu: SKScene {
         for touch in touches {
             
             let location = touch.location(in: self);
-            
-            if nodes(at: location)[0].name == "StartGame" {
-                
- //               GameManager.instance.gameStartedFromMainMenu = true;
-                
-                let scene = GameplayScene(fileNamed: "GameplayScene");
-                scene?.scaleMode = SKSceneScaleMode.aspectFill;
-                self.view?.presentScene(scene!, transition: SKTransition.doorsCloseVertical(withDuration: 1));
-            }
-            
+
             if nodes(at: location)[0].name == "Highscore" {
                 let scene = HighscoreScene(fileNamed: "Highscore");
                 scene?.scaleMode = SKSceneScaleMode.aspectFill;
-                self.view?.presentScene(scene!, transition: SKTransition.doorsCloseVertical(withDuration: 1));
+                print("The Button is Pressed")
+//                self.view?.presentScene(scene!, transition: SKTransition.doorsCloseVertical(withDuration: 1));
             }
             
-            if nodes(at: location)[0].name == "Options" {
-                let scene = Options(fileNamed: "Options");
-                scene?.scaleMode = SKSceneScaleMode.aspectFill;
-                self.view?.presentScene(scene!, transition: SKTransition.doorsCloseVertical(withDuration: 1));
-            }
-            
-            if nodes(at: location)[0].name == "Music" {
-                handleMusicButton();
-            }
-            
+//            if nodes(at: location)[0].name == "Options" {
+//                let scene = Options(fileNamed: "Options");
+//                scene?.scaleMode = SKSceneScaleMode.aspectFill;
+//                self.view?.presentScene(scene!, transition: SKTransition.doorsCloseVertical(withDuration: 1));
+//            }
+//            
+//            if nodes(at: location)[0].name == "Music" {
+//                handleMusicButton();
+//            }
+//            
         }
         
     }
