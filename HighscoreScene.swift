42f0700b610e460169810c264e66b97ce6207b72
Cynthia Strickland <cynthiastrickland001@gmail.com>
2017-02-18 13:44:58 -0800

    getReference and setScore Functions created for HighScore

--- HighscoreScene.swift
+++ HighscoreScene.swift
@@ -10,9 +10,33 @@ import SpriteKit
 
 class HighscoreScene: SKScene {
     
+    private var scoreLabel : SKLabelNode?
+    private var coinLabel : SKLabelNode?
+   
+    
     override func didMove(to view: SKView) {
-        
+        getReference()
     }
+    
+    private func getReference() {
+        scoreLabel = self.childNode(withName: "Score Label") as? SKLabelNode!
+        coinLabel = self.childNode(withName: "Coin Label") as? SKLabelNode!
+    }
+
+    private func setScore() {
+        if GameManager.instance.getEasyDifficulty() == true {
+            scoreLabel?.text = "(\(GameManager.instance.getEasyDifficulty())"
+            coinLabel?.text = "(\(GameManager.instance.getEasyDifficulty())"
+        }
+        else if GameManager.instance.getMediumDifficulty() == true{
+            scoreLabel?.text = "(\(GameManager.instance.getMediumDifficulty())"
+            coinLabel?.text = "(\(GameManager.instance.getMediumDifficulty())"
+        }
+        else if GameManager.instance.getHardDifficulty() == true {
+            scoreLabel?.text = "(\(GameManager.instance.getHardDifficulty())"
+            coinLabel?.text = "(\(GameManager.instance.getHardDifficulty())"
+                }
+            }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
